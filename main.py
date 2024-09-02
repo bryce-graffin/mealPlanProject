64a783bc71e11f72c9832ea1cf70a749b0f86c3f
Bryce Graffin <brycegraffin2019@yahoo.com>
2024-09-01 23:29:54 -0500

    added day selector, reformatted outputs of foods for each meal

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def get_meal_plan(calories, meals):
+def get_meal_plan(calories, num_meals):
     """
     This function takes in user input variables that define the type of meal plan they are looking for. Then uses
     Eat This Much to generate a meal plan for an entire month.
     :param calories: the total number of daily calories that will be used to generate the plan.
-    :param meals: the total number of meals the user wishes to have per day in their plan.
+    :param num_meals: the total number of meals the user wishes to have per day in their plan.
     :return: String - formatted to output the meal plan in a readable fashion.
     """
 
@@ -44,77 +44,88 @@ def get_meal_plan(calories, meals):
     # init the webdriver
     service = Service("/Users/Bryce/Downloads/chromedriver-win64/chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
-    print('Driver initialized...')
+    # print('Driver initialized...')
 
     # get the information from the page
     driver.get(url)
-    print('Locating "https://eatthismuch.com"...')
-    time.sleep(5)
+    # print('Locating "https://eatthismuch.com"...')
+    time.sleep(3)
 
     # tell the website how many calories and meals we want to use
     driver.find_element(By.ID, 'cal_input').send_keys(calories)
     meal_selector = Select(driver.find_element(By.XPATH, '//select[@id="num_meals_selector"]'))
-    meal_selector.select_by_visible_text(f'{meals} meals')
+    meal_selector.select_by_visible_text(f'{num_meals} meals')
     driver.find_element(By.XPATH, '//button[@class="btn btn-lg btn-block btn-orange gen_button"]').click()
 
     # wait for the page to load
-    print('Generating meal plan based on user input...')
+    # print('Generating meal plan based on user input...')
     time.sleep(5)
 
     # find all the meals generated for the set calories and meal count
-    meals = driver.find_elements(By.XPATH, '//div[@class="meal_box meal_container row"]')
-    print('Meals generated.')
+    meals = driver.find_elements(By.XPATH, './/div[@class="meal_box meal_container row"]')
+    # print('Meals generated.\n')
+    # print(f'Meals: {meals}')
 
     meal_list = []  # all meals
+    food_list = []  # all foods (in each meal)
 
     # TODO: make this work for an entire month, add checks for repeat recipes
     # get each meal for the day
-    for meal in range(len(meals)):
-        meal_type = driver.find_element(By.XPATH, './/div[@class="col-auto text-dark-gray text-large text-strong '
-                                                  'print_meal_title wrap_or_truncate pr-0"]')
-        meal_name = driver.find_element(By.XPATH, './/div[@class="print_name"]')
-        meal_calories = driver.find_element(By.XPATH, './/span[@class="cal_amount text-small text-light-gray"]')
-        print(f'\n{meal_type.text}')
-        print(f'Total Calories: {meal_calories.text}\n')
-        print(f'Meal Name: {meal_name}')
-        # get each food in each meal
-        foods = driver.find_elements(By.XPATH, './/li[@class="diet_draggable ui-sortable-handle"]')
+    for index, meal in enumerate(meals):
+        # add the meal information for the meal list
+        meal_name = meal.find_element(By.XPATH, './/div[@class="col-auto text-dark-gray text-large text-strong '
+                                                'print_meal_title wrap_or_truncate pr-0"]')
+        meal_calories = meal.find_element(By.XPATH, './/span[@class="cal_amount text-small text-light-gray"]')
 
-        for food in foods:
-            food_name = food.find_element(By.XPATH, './/div[@class="print_name"]')
-            print(f'Food: {food_name}')
-            # TODO: fix this part for images
-            # food_image = food.find_element(By.XPATH,'.//div[@class="food_image"]')
-            # food_image_url = food_image.get_attribute('style')
-            # food_image_url = food_image_url[food_image_url.index('url(') + 4:]
-            # food_image_url = food_image_url[:food_image_url.index(')')]
-            #
-            # # Print food details
-            # print(f'{food_name.text}')
-            # print(food_image_url)
-            #
-            # # try to display the food image
-            # try:
-            #     response = requests.get(food_image_url)
-            #     img = Image.open(io.BytesIO(response.content))
-            #     img.show()
-            # except Exception as e:
-            #     print(f'Error fetching/displaying image: {e}')
-            # TODO: add something here to look for ingredients
+        meal_list.append(f'\n{meal_name.text}')
+        meal_list.append(meal_calories.text)
 
-        # add the meal for the list
-        meal_list.append(meals[meal].text)
+        # get each of the foods in the corresponding meal
+        meal_foods = meal.find_elements(By.XPATH, './/div[@class="food_name col-12"]/div[@class="print_name"]')
+        for food in range(len(meal_foods)):
+            meal_list.append(f'{meal_foods[food].text}')
+
+    # get each food in the meal
+    foods = driver.find_elements(By.XPATH, './/div[@class="food_name col-12"]/div[@class="print_name"]')
+
+    # TODO: this will be where we check for repeat foods in the plan (within the same week) so we can swap
+    for food in range(len(foods)):
+        food_list.append(foods[food].text)
+        # print(f'Foods:\n{foods[food].text}\n')
 
     # quit the driver and return the meal plan
     driver.quit()
-    return '\n'.join(meal_list)
+    food_list.insert(0, '\nFoods for the day:')
+    return '\n'.join(meal_list + food_list)
 
 
 if __name__ == '__main__':
     # user inputs
-    cals = input('Enter desired daily calories (0-20k): ')
-    num_meals = input('Enter meal count per day (1-9): ')
+    while True:
+        try:
+            days = int(input('Enter how many days you would like to plan for: '))
+            break  # exit the loop if the user enters a valid integer value
+        except ValueError:
+            print("Please enter a valid integer.")
 
-    # get plan
-    meal_plan = get_meal_plan(cals, num_meals)
-    print(meal_plan)
+    while True:
+        try:
+            cals = int(input('Enter desired daily calories (0-20k): '))
+            break  # exit the loop if the user enters a valid integer value
+        except ValueError:
+            print("Please enter a valid integer.")
+
+    while True:
+        try:
+            num_of_meals = int(input('Enter meal count per day (1-9): '))
+            break  # exit the loop if the user enters a valid integer value
+        except ValueError:
+            print("Please enter a valid integer.")
+
+    i = 0  # current day iteration
+
+    # get plan for a month
+    while i < days:
+        meal_plan = get_meal_plan(cals, num_of_meals)
+        print(f'\nDay {i + 1}:\n{meal_plan}')
+        i += 1
